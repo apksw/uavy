15e1811ee166ae6fb711c99fec9d9114e420dcb3
Adrian K <adrian@kugo.tech>
2021-02-22 18:25:32 +0100

    Methods removed

--- auth/pkg/base/service.go
+++ auth/pkg/base/service.go
@@ -3,15 +3,12 @@ package base
 type (
 	Service interface {
 		Name() string
-		Service() Service
-		SetService(Service)
 	}
 )
 
 type (
 	BaseService struct {
-		name    string
-		service Service
+		name string
 	}
 )
 
@@ -27,10 +24,6 @@ func (bs BaseService) Name() string {
 	return bs.name
 }
 
-func (bs BaseService) Service() Service {
-	return bs.service
-}
-
-func (bs BaseService) SetService(s Service) {
-	bs.service = s
+func (bs BaseService) SetName(name string) {
+	bs.name = name
 }
